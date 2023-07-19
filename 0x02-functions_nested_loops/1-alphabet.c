38a7fc8d5338b1472dd3a1e2300abee9dc9acb2d
abdelrahmanmorgan22 <morganabdo08@gmail.com>
2023-07-18 20:02:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,14 @@
 */
 
 void print_alphabet(void)
+
 {
+
             int letter;
             for (letter = 'a'; letter <= 'z'; letter++)
             {
             _putchar (letter);
             }
             _putchar (10);
+
 }
